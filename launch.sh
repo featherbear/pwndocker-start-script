f3ddc8d7a4385e06011f040092b10e819ed0cdd2
Andrew Wong <andrew.j.wong@outlook.com>
2020-07-28 14:29:54 +1000

    Fix escaping

--- launch.sh
+++ launch.sh
@@ -3,8 +3,8 @@
 echo "Launching pwndocker"
 
 cat > /tmp/.pwnDockerBashAliases << :EOF
-gropper () { ropper -f "$1" --nocolor | grep "$2"; }
-export PS1="\n--------------------\n\w \T \$?\n\\$ \[$(tput sgr0)\]"
+gropper () { ropper -f "\$1" --nocolor | grep "\$2"; }
+export PS1="\n--------------------\n\w \T \$?\n\\\\$ \[\$(tput sgr0)\]"
 :EOF
 
 sg docker 'cd $(pwd); docker run -d --rm -h banana --name banana -v /tmp/.pwnDockerBashAliases:/root/.bash_aliases  -v $(pwd):/ctf/work -p 23946:23946 --cap-add=SYS_PTRACE skysider/pwndocker@sha256:7a91fcee7f34679a40bef80f82a2872298530b0c78c2e0ac9fd183c15fe60ba2 >/dev/null 2>&1'
